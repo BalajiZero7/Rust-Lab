37d3c5462ce48be25e86dff27c4c7e36f0783f2f
BalajiZero7 <imbalaji.s@gmail.com>
2018-02-04 20:38:54 +0530

    Prime funtion added

--- prime/src/main.rs
+++ prime/src/main.rs
@@ -2,11 +2,11 @@ fn is_prime(n: u64) -> bool {
     match n {
         0...1 => false,
         _ => (!(2..n).any(|d| n % d == 0)),
-   
 }
 }
 
 fn main() {
-    let a = is_prime(6);
-    println!("Is the given number is a prime number? : {}", a);  
+    let n = 6;
+    let a = is_prime(n);
+    println!("Is {} given number is a prime number? : {}", n, a);  
 }
